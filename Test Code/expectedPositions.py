6b7b165182e6d137818bac87b739829e37df07e8
ble61 <ble61@uclive.ac.nz>
2024-04-04 13:54:31 +1300

    skybot and MPC tracks

--- Test Code/expectedPositions.py
+++ Test Code/expectedPositions.py
@@ -15,6 +15,7 @@ Last Edited 2/4/24
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mplc
 import itertools
 from astropy.coordinates import SkyCoord
 import astropy.units as u
@@ -46,16 +47,17 @@ def plotExpectedPos(posArr, times, lims, names):
     ax.set_ylim([lims[2],lims[3]])
 
     markers = itertools.cycle((".","x","^","*","D","+","v","o", "<", ">","H","1","2","3","4","8","X","p","d","s","P","h")) #marker cycle
-    
+    cnorm = mplc.Normalize(np.min(times), np.max(times))
     #plots each asteroid
     for i in range(posArr.shape[0]):
-        ax.scatter(posArr[i,0],posArr[i,1], marker=next(markers), c=times, cmap="plasma", label = names[i])
+        ax.plot(posArr[i,0],posArr[i,1], c="grey", alpha=0.4)
+        ax.scatter(posArr[i,0],posArr[i,1], c=times, marker=next(markers), cmap="plasma", norm=cnorm, label = names[i])
     
     plt.scatter(ra_i, dec_i, marker="+", s=100, c="g") #Center of seach area
 
     # ax.legend() #! Not really working, stops after a few symbols
 
-    fig.savefig(f"./posTracks_ra{ra_i}_dec{dec_i}_t{t_i}.png")
+    # fig.savefig(f"./posTracks_ra{ra_i}_dec{dec_i}_t{t_i.mjd}.png")
 
 #sets up positions and time
 #TODO get _i vals as inputs?    
